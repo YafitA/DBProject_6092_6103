bb63a6581b0aee8ef60449a3f6e359852aa0ca63
YafitA <154600173+YafitA@users.noreply.github.com>
2025-04-28 14:17:29 +0300

    Update Constraints.sql

--- Phase B/Constraints.sql
+++ Phase B/Constraints.sql
@@ -6,7 +6,7 @@ UNIQUE (ShiftDate, StartTime, EndTime);
 -- CHECK constraint on the Volunteer table: Phone number must be at least 9 digits long
 ALTER TABLE Volunteer
 ADD CONSTRAINT chk_phone_length
-CHECK (LENGTH(PhoneNumber) >= 9);
+CHECK (LENGTH(PhoneNumber) >= 9 AND LENGTH(PhoneNumber) <= 10);
 
 -- DEFAULT constraint on the Project table: Default start date is today's date
 ALTER TABLE Project
